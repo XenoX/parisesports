11fadae8bf4880e8c4d92b587f517e7c56f81c8e
XenoX <xenox1337@gmail.com>
2018-10-09 18:32:52 +0200

    First commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @Template
+     */
+    public function index()
+    {
+        return [];
+    }
+}
